ab7db733fcbe7674ee97250a4e4876729a00ad22
himaniprajapati <himani2908@gmail.com>
2016-11-22 23:21:17 +0530

    main_with_all_test_cases

--- main.py
+++ main.py
@@ -1,56 +1,31 @@
-from pyeda.inter import *
-from helper import *
-from math import *
-def McCluskey(m,Nbits):
+from pyeda.inter import * 
+from SOP_POS_final import *
+from SOP_POS_comple import *
+from POS_SOP_final import *
+from POS_SOP_comple import *
+from SOP_ESOP import *
 
-    print("__________________Mc CLUSKEY_____________________")
-    print("MINTERM INPUT  (BITS :",Nbits,"): ",m)
-    mintermBinary = minterm2BinString(m,Nbits) # convert minterms into their binary strings
-    P = sortNumOne(mintermBinary)              # create the table P with minterm ( STORE PRIME TO MINTERM MAPPING)
-    dispX(P,"M","MintermBinaryTable")
-    McTable = [P]
-    mergeTable(McTable,0)       # merge and find all possible minterms
-    dispX(McTable,"McT","Mc Table Merged")
-    primes = getPrimes(McTable)    # get the primes from the table ( values marked as Y)
-    dispX(primes,"Pr","Prime Implicants")
-    S = getS(m,primes) # sort the inverse of STORE MINTERM TO PRIME MAPPING
-    dispX(S,"S","MINTERM TO PRIME MAPPING (INPUT)")
-    print("________PRIME IMPLICANT TABLE_________")
-    displayPrimeImplicantTable(m, primes)
-    essentialPrimes = tableReduce(m, primes)
+#TEST_CASE Printing all canonical forms
+print(" ")
+f=expr("a|(~b&c)|(~b&~d)")
+print (f)
+print ("SOP_to_POS_comple:",SOP_to_POS_comple(f))
+print (" ")
+f=expr("a&(~c|v)&(~d|~a)")
+print (f)
+print ("POS_to_SOP_comple:",POS_to_SOP_comple(f))
+print (" ")
+f=expr("~x1&(~x2|x3)&(x2|x5)")
+print (f)
+print("POS_to_SOP:",POS_SOP_direct(f))
+print (" ")
+f=expr("a|(b&c)|(~b&d)")
+print (f)
+print("SOP_to_POS:",SOP_POS_indirect(f))
+print(" ")
+f=expr("~x1|x2&~x3|x3&x4")
+print (f)
+g=SOP_to_ESOP(f)
+print ("SOP_to_ESOP:",SOP_to_ESOP(f))	
+print(" ")
 
-    return essentialPrimes
-def getEssentialImplicants(f,X):
-    #f_bar = f # complement the function to get the minterms
-    n = len(X)  # number of variables
-    # SHANNON METHOD FOR FINDING MINTERMS
-    m=''
-    M =[]
-    shannon(f,X,0,n,m,M)
-    print("M",M)
-    minterms = [ int(x,2) for x in M]
-    print("______________SHANNON EXPANSION_______________")
-    print("Expression :",f)
-    print("Minterms :",minterms)
-    m = minterms
-    # MCCLUSKY ALGORITHM FOR REDUCING ON MINTERMS AND FINDING PRIME IMPLICANTS  TO ESSENTIAL PRIME  IMPLICANTS
-    m = list(set(m))     # make sure all minterms are unique
-    Nbits = ceil(log(max(m)+1,2)) # Get number of Bits to work with
-    essentialPrimes = McCluskey(m,Nbits) # call McCluksy Function for  getting essential prime implicants
-    essentialPrimes = set(essentialPrimes) # remove  duplicates
-    print("Essential Prime Implicants :", essentialPrimes)
-    return essentialPrimes
-
-
-#-----------SHANNON--------------
-#  Enter  the espression here
-fStr = 'x[0]&x[1]|x[0]&~x[1]'
-f = expr(fStr)
-
-n =3
-X = exprvars('x', n)
-ep = getEssentialImplicants(f,X)
-SOP = ''
-for s in prime2Str(ep):
-    SOP = '('+s+')+'+SOP
-print('SOP:',SOP)
